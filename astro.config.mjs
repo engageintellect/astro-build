2c474ac065cbb4bc8f6bffcf06360e9ff97c7d79
engageintellect <jc9361@gmail.com>
2023-03-18 11:40:08 -0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercelEdge from "@astrojs/vercel/edge";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), react()]
+  integrations: [svelte(), tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
--- src/pages/buld.json.ts
+++
@@ -1,9 +0,0 @@
-// Outputs: /builtwith.json
-export async function get({ params, request }) {
-  return {
-    body: JSON.stringify({
-      name: "Astro",
-      url: "https://astro.build/",
-    }),
-  };
-}
